1cb916e6569d841b0eb3fe4511119486a063cf6a
pauloeduardods <pauloedsg@gmail.com>
2024-05-15 23:59:13 -0300

    Refac: camera interface

--- internal/domain/camera/camera.go
+++ internal/domain/camera/camera.go
@@ -9,7 +9,15 @@ import (
 type Camera interface {
 	Start() error
 	Stop() error
-	GetDimensions() (int, int, error)
+	GetCapabilities() (CameraCapabilities, error)
 	RecordVideo(ctx context.Context, filename string) error
 	Capture() (*gocv.Mat, error)
+	SetFPS(fps float64) error
+}
+
+type CameraCapabilities struct {
+	DeviceID int
+	Width    int
+	Height   int
+	FPS      float64
 }
