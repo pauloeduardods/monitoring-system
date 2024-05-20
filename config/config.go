ce2054097d93e372ab55ee92bdb51284cdfd1962
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2024-05-20 17:42:27 -0300

    Refac: Update config to use sqlite

--- config/config.go
+++ config/config.go
@@ -1,67 +1,133 @@
 package config
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
-	"os"
-	"strconv"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Config struct {
-	AWSRegion    string
+type AwsConfig struct {
+	Region       string
 	S3BucketName string
-	DeviceID     []int
-	Host         string
-	Port         int
 }
 
-func NewConfig() (*Config, error) {
-	region := os.Getenv("AWS_REGION")
-	if region == "" {
-		return nil, fmt.Errorf("AWS_REGION environment variable is not set")
-	}
+type ApiConfig struct {
+	Host string
+	Port int
+}
 
-	s3BucketName := os.Getenv("S3_BUCKET_NAME")
-	if s3BucketName == "" {
-		return nil, fmt.Errorf("S3_BUCKET_NAME environment variable is not set")
-	}
+type Config struct {
+	Aws AwsConfig
+	Api ApiConfig
+}
 
-	host := os.Getenv("HOST")
-	if host == "" {
-		return nil, fmt.Errorf("HOST environment variable is not set")
-	}
+type ConfigManager struct {
+	db *sql.DB
+}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		return nil, fmt.Errorf("PORT environment variable is not set")
-	}
+func NewConfigManager(configDb *sql.DB) (*ConfigManager, error) {
+	createAWSConfigTable := `
+	CREATE TABLE IF NOT EXISTS aws_config (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		region TEXT NOT NULL,
+		s3_bucket_name TEXT NOT NULL
+	);`
 
-	portInt, err := strconv.Atoi(port)
+	createApiConfigTable := `
+	CREATE TABLE IF NOT EXISTS api_config (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		host TEXT NOT NULL,
+		port INTEGER NOT NULL
+	);`
+
+	_, err := configDb.Exec(createAWSConfigTable)
 	if err != nil {
-		return nil, fmt.Errorf("error converting PORT to int: %v", err)
+		return nil, err
 	}
 
-	deviceID := []int{0}
+	_, err = configDb.Exec(createApiConfigTable)
+	if err != nil {
+		return nil, err
+	}
+
+	err = insertDefaultConfigs(configDb)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConfigManager{db: configDb}, nil
+}
+
+func (cm *ConfigManager) LoadConfig() (*Config, error) {
+	awsConfig, err := cm.loadAWSConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	apiConfig, err := cm.loadApiConfig()
+	if err != nil {
+		return nil, err
+	}
 
 	return &Config{
-		AWSRegion:    region,
-		S3BucketName: s3BucketName,
-		DeviceID:     deviceID,
-		Host:         host,
-		Port:         portInt,
+		Aws: *awsConfig,
+		Api: *apiConfig,
 	}, nil
 }
 
-func NewAWSConfig(ctx context.Context, env *Config) (*aws.Config, error) {
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion(env.AWSRegion),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("error loading aws configuration: %v", err)
+func (cm *ConfigManager) loadAWSConfig() (*AwsConfig, error) {
+	row := cm.db.QueryRow("SELECT region, s3_bucket_name FROM aws_config LIMIT 1")
+	var region, s3BucketName string
+	if err := row.Scan(&region, &s3BucketName); err != nil {
+		return nil, fmt.Errorf("error loading AWS config: %v", err)
+	}
+	return &AwsConfig{Region: region, S3BucketName: s3BucketName}, nil
+}
+
+func (cm *ConfigManager) loadApiConfig() (*ApiConfig, error) {
+	row := cm.db.QueryRow("SELECT host, port FROM api_config LIMIT 1")
+	var host string
+	var port int
+	if err := row.Scan(&host, &port); err != nil {
+		return nil, fmt.Errorf("error loading API config: %v", err)
+	}
+	return &ApiConfig{Host: host, Port: port}, nil
+}
+
+func (cm *ConfigManager) SaveAWSConfig(config *AwsConfig) error {
+	_, err := cm.db.Exec("INSERT INTO aws_config (region, s3_bucket_name) VALUES (?, ?)", config.Region, config.S3BucketName)
+	return err
+}
+
+func (cm *ConfigManager) SaveApiConfig(config *ApiConfig) error {
+	_, err := cm.db.Exec("INSERT INTO api_config (host, port) VALUES (?, ?)", config.Host, config.Port)
+	return err
+}
+
+func insertDefaultConfigs(db *sql.DB) error {
+	row := db.QueryRow("SELECT COUNT(*) FROM aws_config")
+	var count int
+	if err := row.Scan(&count); err != nil {
+		return fmt.Errorf("error checking AWS config table: %v", err)
+	}
+	if count == 0 {
+		_, err := db.Exec("INSERT INTO aws_config (region, s3_bucket_name) VALUES (?, ?)", "us-east-1", "golang-camera")
+		if err != nil {
+			return fmt.Errorf("error inserting default AWS config: %v", err)
+		}
 	}
 
-	return &cfg, nil
+	row = db.QueryRow("SELECT COUNT(*) FROM api_config")
+	if err := row.Scan(&count); err != nil {
+		return fmt.Errorf("error checking API config table: %v", err)
+	}
+	if count == 0 {
+		_, err := db.Exec("INSERT INTO api_config (host, port) VALUES (?, ?)", "localhost", 3000)
+		if err != nil {
+			return fmt.Errorf("error inserting default API config: %v", err)
+		}
+	}
+
+	return nil
 }
